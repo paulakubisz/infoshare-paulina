d59690ee9aa62ea2f204f38a6e7a24def5deb1c4
Paulina Kubisz <paulina.kubisz@airhelp.com>
2018-03-15 16:37:10 +0100

    Zadanie domowe: zmień zadania z while na for.

---
+++ project/Day4/while2.py
@@ -0,0 +1,18 @@
+#
+
+wejscie = input("Podaj mi liczbę: ")
+
+while not wejscie.isdigit():
+    print('Tylko cyfry!')
+    wejscie = input("Podaj liczbę: ")
+
+print(int(wejscie)** 2) # zamiana liczby do kwadratu
+
+#DRUGI SPOSOB
+
+for cos in wejscie:
+    if not cos.isdigit():
+        break
+
+print(int(wejscie) ** 2)  # zamiana liczby do kwadratu
+
