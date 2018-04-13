cccc1d21d5b0526c540027f8cbce6b0e56b44c4e
Paulina Kubisz <paulina.kubisz@airhelp.com>
2018-04-13 22:58:27 +0200

    magazyn part. 1

--- Sold/program.py
+++ Sold/program.py
@@ -1,5 +1,10 @@
-from Sold.yesornot import  query_yes_no, enter_product
+from Sold.yesornot import enter_product, query_yes_no, find_product, update_data
 
 
-enter_product()
-query_yes_no()
+# enter_product()
+#
+# query_yes_no("Do you want add more? ")
+#
+# find_product("What are you looking for? ")
+
+update_data()
--- Sold/yesornot.py
+++ Sold/yesornot.py
@@ -1,12 +1,11 @@
 
-
 def enter_product():
     """
     Enter name and amount of product.
     All product will sign up into file magazyn.csv as list.
 
     """
-    n = int(input('How many item do you enter? '))          #n is the number of items you want to enter
+    n = int(input('How many product do you enter? '))          #n is the number of items you want to enter
     db =[]
     for i in range(n):
         a = input("Enter the name of product and amount: ")
@@ -20,18 +19,50 @@ def enter_product():
         file.writelines(db)
 
 
-
-def query_yes_no(question, default="n"):
+def find_product(p):
     """
-
+    Enter the name product which you are looking for.
+    Then you get back information how many products are in magazine.
 
     """
+    import re
+
+    s = input(p)
+    file = open('magazyn.csv', 'r')
+
+    for line in file:
+        if re.match(s,line):
+            print(f"I found: ", line)
+            break
+    else:
+        print(f"I can't find {s} in magazyn.csv")
+        query_yes_no('Do you want add more?')
+
+def update_data():
+
+    file = open('magazyn.csv', 'r+')
+    d = input("Do you want update a data? Press y or n!")
+
+    if d == "y":
+        for line in file:
+            product_for_delete = input("Enter product? ")
+            find_product(product_for_delete)
+
+
+
+
+    elif d == "n":
+        print("Thank You for using my magazin :) ")
+
+
+
+
+def query_yes_no(q) -> object:
     while True:
-        from pip._vendor.distlib.compat import raw_input
-        question = raw_input("Are you finish?" "Press: (y/n)" + "\n")
-        if question == "n":
+        question = input(q + "Press: (y/n)" + "\n")
+        if question == "y":
             enter_product()
-        elif question == "y":
+        elif question == "n":
             print("All data is saving in magazyn.csv file")
             break
         else:
